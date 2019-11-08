929429ce7c895d1b6002412b91c63769f056f67a
lendres <lendres@fifthrace.com>
2019-11-08 09:41:38 -0600

    Added item templates

---
+++ Class/Class.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace $rootnamespace$
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class $safeitemrootname$
+	{
+		#region Enumerations
+
+		#endregion
+
+		#region Delegates
+
+		#endregion
+
+		#region Events
+
+		#endregion
+
+		#region Members
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public $safeitemrootname$() {}
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region XML
+
+		#endregion
+
+	} // End class.
+} // End namespace.
---
+++ FixedDialogForm/FixedDialogForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace $rootnamespace$
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class $safeitemname$ : Form
+	{
+		#region Members
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public $safeitemname$()
+		{
+			InitializeComponent();
+
+			PopulateControls();
+		}
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Event Handlers
+
+		/// <summary>
+		/// Ok button event handler.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">Event arguments.</param>
+		private void buttonOK_Click(object sender, EventArgs e)
+		{
+			// TODO: Validation code goes here.
+
+			PushEntriesToDataStructure();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Initialize the controls with the values from the data structure.
+		/// </summary>
+		protected void PopulateControls()
+		{
+
+		}
+
+		/// <summary>
+		/// Save the data back to the data structure.
+		/// </summary>
+		protected void PushEntriesToDataStructure()
+		{
+
+		}
+
+		#endregion
+
+	} // End class.
+} // End namespace.
